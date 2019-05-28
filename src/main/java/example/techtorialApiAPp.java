7096f736c4301ba3098c097596206f44fcc9aada
admin <admin@admins-MacBook-Pro.local>
2019-05-27 21:03:04 -0700

    Initial commit

---
+++ src/main/java/example/techtorialApiAPp.java
@@ -0,0 +1,14 @@
+package example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class techtorialApiAPp {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(techtorialApiAPp.class,args);
+    }
+
+}
